a9e93c6fe48c9d1bf37c4ed67eea437610ff41bb
Tom Craven <tom.cass.craven@gmail.com>
2016-05-20 21:00:37 +0100

    printing the table headers

--- table.go
+++ table.go
@@ -1,9 +1,16 @@
 package gotable
 
+import "strings"
+
+const (
+	columnChar = "|"
+	rowChar    = "-"
+	cornerChar = "+"
+)
+
 // Table holds the table state
 type Table struct {
 	columns []Column
-	output  Output
 }
 
 // NewTable creates a new table
@@ -15,5 +22,30 @@ func NewTable(columns []Column) Table {
 
 // Print outputs the table to the output object
 func (t *Table) Print(output Output) {
+	outputBuffered := NewOutputBuffered(output)
+	t.printHeader(outputBuffered)
+}
 
+func (t *Table) printHeader(output OutputBuffered) {
+	t.printHorizontalSeparator(output)
+	t.printColumnHeaders(output)
+	t.printHorizontalSeparator(output)
+}
+
+func (t *Table) printColumnHeaders(output OutputBuffered) {
+	output.Print(columnChar)
+	for _, column := range t.columns {
+		column.PrintHeader(&output)
+		output.Print(columnChar)
+	}
+	output.Flush()
+}
+
+func (t *Table) printHorizontalSeparator(output OutputBuffered) {
+	output.Print(cornerChar)
+	for _, column := range t.columns {
+		output.Print(strings.Repeat(rowChar, column.getWidth())).
+			Print(cornerChar)
+	}
+	output.Flush()
 }
--- table_test.go
+++ table_test.go
@@ -1,11 +1,122 @@
 package gotable_test
 
 import (
+	"github.com/golang/mock/gomock"
 	. "github.com/tomcraven/gotable"
+	. "github.com/tomcraven/gotable/gotable_mock"
 )
 
 var _ = Describe("Table", func() {
 	It("creates a blank table", func() {
 		_ = NewTable([]Column{})
+	})
+
+	Describe("Push", func() {
+		It("pushes a row into the table", func() {
+
+		})
+	})
+
+	Describe("Print", func() {
+		type tableConfiguration struct {
+			columns        []Column
+			expectedOutput []string
+		}
+
+		var tableConfigurations []tableConfiguration
+
+		printTest := func() {
+			var (
+				t              Table
+				mockController *gomock.Controller
+				mockOutput     *MockOutput
+			)
+
+			for _, tableConfig := range tableConfigurations {
+				BeforeEach(func() {
+					t = NewTable(tableConfig.columns)
+					mockController = gomock.NewController(GinkgoT())
+					mockOutput = NewMockOutput(mockController)
+				})
+
+				It("prints correctly", func() {
+					for _, expectedOutput := range tableConfig.expectedOutput {
+						mockOutput.EXPECT().Print(expectedOutput)
+					}
+					t.Print(mockOutput)
+				})
+
+				AfterEach(func() {
+					mockController.Finish()
+				})
+			}
+		}
+
+		Context("when a table has a single column", func() {
+			tableConfigurations = []tableConfiguration{
+				{
+					columns: []Column{
+						NewColumn("test", 4),
+					},
+					expectedOutput: []string{
+						"+----+",
+						"|test|",
+						"+----+",
+					},
+				},
+				{
+					columns: []Column{
+						NewColumn("another test", 20),
+					},
+					expectedOutput: []string{
+						"+--------------------+",
+						"|    another test    |",
+						"+--------------------+",
+					},
+				},
+				{
+					columns: []Column{
+						NewColumn("test odd", 9),
+					},
+					expectedOutput: []string{
+						"+---------+",
+						"|test odd |",
+						"+---------+",
+					},
+				},
+			}
+
+			printTest()
+		})
+
+		Context("when a table has multiple columns", func() {
+			tableConfigurations = []tableConfiguration{
+				{
+					columns: []Column{
+						NewColumn("test", 4),
+						NewColumn("test", 4),
+					},
+					expectedOutput: []string{
+						"+----+----+",
+						"|test|test|",
+						"+----+----+",
+					},
+				},
+				{
+					columns: []Column{
+						NewColumn("col", 5),
+						NewColumn("middle column", 20),
+						NewColumn("c", 1),
+					},
+					expectedOutput: []string{
+						"+-----+--------------------+-+",
+						"| col |   middle column    |c|",
+						"+-----+--------------------+-+",
+					},
+				},
+			}
+
+			printTest()
+		})
 	})
 })
