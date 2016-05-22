43048ec7d5d38584a49cbb53366f7995d1f4f795
Tom Craven <tom.cass.craven@gmail.com>
2016-05-22 17:06:11 +0100

    use cells to format the header

--- column.go
+++ column.go
@@ -1,7 +1,5 @@
 package gotable
 
-import "strings"
-
 // Column contains information about a column in a Table
 type Column interface {
 	PrintHeader(Output)
@@ -11,29 +9,23 @@ type Column interface {
 }
 
 type columnImpl struct {
-	name  string
-	width int
-	cells []Cell
+	header Cell
+	width  int
+	cells  []Cell
 }
 
 // NewColumn instantiates and returns a new instance of Column
 func NewColumn(name string, width int) Column {
-	return &columnImpl{
-		name:  name,
+	newColumn := &columnImpl{
 		width: width,
 	}
+	newColumn.header = NewAlignedCell(newColumn, name, centre)
+	return newColumn
 }
 
-// PrintHeader prints the column header
-// Currently only supports aligning to centre
+// PrintHeader prints the column header aligned to centre
 func (c *columnImpl) PrintHeader(output Output) {
-	// TODO: can this be done with cells?
-
-	nameLength := len(c.name)
-	spareRoom := c.GetWidth() - nameLength
-	leftPadding := strings.Repeat(" ", spareRoom/2)
-	rightPadding := strings.Repeat(" ", spareRoom-(spareRoom/2))
-	output.Print(leftPadding + c.name + rightPadding)
+	c.header.Print(output)
 }
 
 // PrintCellAt takes an ordinal and the output interface and prints the row
