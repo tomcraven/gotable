7b9230cb1e581d888beebdda8a672837b819afce
Tom Craven <tom.cass.craven@gmail.com>
2016-05-22 17:05:55 +0100

    add support for creating cells with custom alignment

--- cell.go
+++ cell.go
@@ -8,28 +8,48 @@ import (
 // Cell a single cell in a column/row
 type Cell interface {
 	Print(Output)
+	SetAlignment(alignment)
 }
 
 // NewCell creates a new cell based off the input type
 func NewCell(column Column, x interface{}) Cell {
+	return NewAlignedCell(column, x, unset)
+}
+
+// NewAlignedCell creates a cell with a specified alignment
+// pass 'unset' to use default alignment, or simply call NewCell
+func NewAlignedCell(column Column, x interface{}, align alignment) Cell {
 	switch x.(type) {
 	case int:
 		return intCell{
-			item:   x.(int),
-			column: column,
+			baseCell: baseCell{
+				column:    column,
+				alignment: getAlignment(align, right),
+			},
+			item: x.(int),
 		}
 	case string:
 		return stringCell{
-			item:   x.(string),
-			column: column,
+			baseCell: baseCell{
+				column:    column,
+				alignment: getAlignment(align, left),
+			},
+			item: x.(string),
 		}
 	default:
 		panic("unsupported cell format")
 	}
 }
 
+func getAlignment(a, backup alignment) alignment {
+	if a == unset {
+		return backup
+	}
+	return a
+}
+
 // --------------------
-// Helpers
+// Cell helpers
 // --------------------
 
 func lPad(str string, width int) string {
@@ -50,27 +70,77 @@ func rPad(str string, width int) string {
 	return str + strings.Repeat(" ", width-strLen)
 }
 
+func centrePad(str string, width int) string {
+	strLen := len(str)
+	if strLen >= width {
+		return str
+	}
+
+	spareRoom := width - strLen
+	leftPadding := strings.Repeat(" ", spareRoom/2)
+	rightPadding := strings.Repeat(" ", spareRoom-(spareRoom/2))
+	return leftPadding + str + rightPadding
+}
+
+func padForAlignment(str string, width int, a alignment) string {
+	switch a {
+	case left:
+		return rPad(str, width)
+	case right:
+		return lPad(str, width)
+	case centre:
+		return centrePad(str, width)
+	}
+
+	// Should not reach here but add a default return value anyway
+	return centrePad(str, width)
+}
+
+type alignment int
+
+const (
+	unset alignment = iota
+	left
+	right
+	centre
+)
+
 // --------------------
 // Cells
 // --------------------
 
+type baseCell struct {
+	column    Column
+	alignment alignment
+}
+
+func (c baseCell) SetAlignment(a alignment) {
+	c.alignment = a
+}
+
+// --------------------
+
 type intCell struct {
-	item   int
-	column Column
+	baseCell
+	item int
 }
 
 func (c intCell) Print(output Output) {
 	str := strconv.Itoa(c.item)
-	output.Print(lPad(str, c.column.GetWidth()))
+	output.Print(
+		padForAlignment(str, c.column.GetWidth(), c.alignment),
+	)
 }
 
 // --------------------
 
 type stringCell struct {
-	item   string
-	column Column
+	baseCell
+	item string
 }
 
 func (c stringCell) Print(output Output) {
-	output.Print(rPad(c.item, c.column.GetWidth()))
+	output.Print(
+		padForAlignment(c.item, c.column.GetWidth(), c.alignment),
+	)
 }
