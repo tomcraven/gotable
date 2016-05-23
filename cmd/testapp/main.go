8c940dbc93a43f97fb220092f14f1bffd1df7842
Tom Craven <tom.cass.craven@gmail.com>
2016-05-23 23:20:02 +0100

    add example of passing a custom cell structure to table

--- cmd/testapp/main.go
+++ cmd/testapp/main.go
@@ -2,10 +2,17 @@ package main
 
 import "github.com/tomcraven/gotable"
 
+type customCell struct {
+}
+
+func (c *customCell) Print(output gotable.Output) {
+	output.Print("custom cell")
+}
+
 func main() {
 	t := gotable.NewTable([]gotable.Column{
 		gotable.NewColumn("test", 10),
-		gotable.NewColumn("test2", 8),
+		gotable.NewColumn("test2", 20),
 	})
 
 	t.Push(23, 42)
@@ -13,18 +20,20 @@ func main() {
 	t.Push(true, false)
 	t.Push(123456789987654321, "this line is too long")
 	t.Push(23.42, 42.23)
+	t.Push(&customCell{}, &customCell{})
 
 	t.Print()
 
 	/* Output:
-	+----------+--------+
-	|   test   | test2  |
-	+----------+--------+
-	|        23|      42|
-	|hello     |world   |
-	|true      |false   |
-	|1234567899|this lin|
-	|     23.42|   42.23|
-	+----------+--------+
+	+----------+--------------------+
+	|   test   |       test2        |
+	+----------+--------------------+
+	|        23|                  42|
+	|hello     |world               |
+	|true      |false               |
+	|1234567899|this line is too lon|
+	|     23.42|               42.23|
+	|custom cel|custom cell         |
+	+----------+--------------------+
 	*/
 }
